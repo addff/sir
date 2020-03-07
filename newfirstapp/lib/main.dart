2c7be85e368b6bcff925da8cff147c6fdf251672
Shahadan Bin Saad <shahadansaad@gmail.com>
2020-03-07 12:06:17 +0800

    row

--- newfirstapp/lib/main.dart
+++ newfirstapp/lib/main.dart
@@ -12,6 +12,74 @@ class MyApp extends StatelessWidget {
           title: Text('Senang Jer'),
         ),
         body: Container(
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.purple[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.blue[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.orange[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.amber[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.lime[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.all(10.0),
+                    color: Colors.green[600],
+                    width: 48.0,
+                    height: 48.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        /*
+        body: Container(
           child: Center(
             child: Container(
               margin: const EdgeInsets.all(10.0),
@@ -21,6 +89,7 @@ class MyApp extends StatelessWidget {
             ),
           )
         ),
+        */
       ),
     );
   }
